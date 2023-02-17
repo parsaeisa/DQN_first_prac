cc695c5a69a1c2726946529ab871b58c2045ad18
parsaeisa <parsaeisa@github.com>
2023-02-17 17:16:58 +0330

    write iterations

--- dqn-1.py
+++ dqn-1.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Activation, Flatten
 from keras.optimizers import Adam
 from collections import deque
+from tqdm import tqdm
 
 import time
 import os
@@ -51,7 +52,8 @@ class DQNAgent:
         # We take a random sample from this memory and give it to the network as a batch
         self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
 
-        self.tensorboard = self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
+        self.tensorboard = self.tensorboard = ModifiedTensorBoard(
+            log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
 
     def create_model(self):
         model = Sequential()
@@ -80,7 +82,7 @@ class DQNAgent:
         self.replay_memory.append(transition)
 
     def get_qs(self, state, step):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+        return self.model.predict(np.array(state).reshape(-1, *state.shape) / 255)[0]
 
     def train(self, terminal_state, step):
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
@@ -91,7 +93,7 @@ class DQNAgent:
         current_states = np.array([transition[0] for transition in minibatch]) / 255
         current_qs_list = self.model.predict(current_states)
 
-        new_current_states = np.array([transition[3] for transition in minibatch])/255
+        new_current_states = np.array([transition[3] for transition in minibatch]) / 255
         future_qs_list = self.target_model.predict(new_current_states)
 
         x = []
@@ -112,7 +114,7 @@ class DQNAgent:
             x.append(current_state)
             y.append(current_qs)
 
-        self.model.fit(np.array(x)/255, np.array(y)/255, batch_size=MINIBATCH_SIZE
+        self.model.fit(np.array(x) / 255, np.array(y) / 255, batch_size=MINIBATCH_SIZE
                        , verbose=0, shuffle=False,
                        callbacks=[self.tensorboard] if terminal_state else None)
         # * Shuffle is false because we already randomly sampled data from replay memory
@@ -123,6 +125,7 @@ class DQNAgent:
         if self.target_update_counter > UPDATE_TARGET_EVERY:
             self.target_model.set_weights(self.model.get_weights())
             self.target_update_counter = 0
+
 
 env = BlobEnv()
 
@@ -135,9 +138,56 @@ np.random.seed(1)
 tf.set_random_seed(1)
 
 # Memory fraction, used mostly when trai8ning multiple agents
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
-#backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
+# backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
 
 # Create models folder
 if not os.path.isdir('models'):
     os.makedirs('models')
+
+agent = DQNAgent()
+
+for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit="episode"):
+    agent.tensorboard.step = episode
+    episode_reward = 0
+    step = 1
+    current_state = env.reset()  # I think this is the initial state
+    done = False
+
+    while not done:
+        if np.random.random() > epsilon:
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+
+        new_state, reward, done = env.step(action)
+
+        episode_reward += reward
+
+        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+            env.render()
+
+        agent.update_replay_memory((current_state, action, reward, new_state, done))
+        agent.train(done, step)
+
+        current_state = new_state
+        step += 1
+
+    # Append episode reward to a list and log stats (every given number of episodes)
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward,
+                                       epsilon=epsilon)
+
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(
+                f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
